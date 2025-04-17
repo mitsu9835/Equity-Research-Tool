0d2c547a71e1cf5c4b2198070f0e586813a05078
Sumit <raj.sumit9835@gmail.com>
2025-04-17 20:41:45 +0530

    changed number of url links from 3 to 5

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ st.title("News Research Tool 📈")
 st.sidebar.title("News Article URLs")
 
 urls = []
-for i in range(3):
+for i in range(5):
     url = st.sidebar.text_input(f"URL {i+1}")
     urls.append(url)
 
